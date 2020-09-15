df62eccaab71ccd53677b3e1692e1b3e39fb6b71
Alexander Swed <lexswed@gmail.com>
2020-09-15 22:45:57 +0200

    update palette

--- lib/stitches.config.ts
+++ lib/stitches.config.ts
@@ -1,4 +1,5 @@
 import { createStyled } from '@stitches/react';
+import colors from './theme/colors';
 
 const scales = {
   $0: '0px',
@@ -22,67 +23,7 @@ const scales = {
 };
 
 export const theme = {
-  colors: {
-    $gray100: '#F7FAFC',
-    $gray200: '#EDF2F7',
-    $gray300: '#E2E8F0',
-    $gray400: '#CBD5E0',
-    $gray500: '#A0AEC0',
-    $gray600: '#718096',
-    $gray700: '#4A5568',
-    $gray800: '#2D3748',
-    $gray900: '#1A202C',
-
-    $orange100: '#FFFAF0',
-    $orange200: '#FEEBC8',
-    $orange300: '#FBD38D',
-    $orange400: '#F6AD55',
-    $orange500: '#ED8936',
-    $orange600: '#DD6B20',
-    $orange700: '#C05621',
-    $orange800: '#9C4221',
-    $orange900: '#7B341E',
-
-    $yellow100: '#FFFFF0',
-    $yellow200: '#FEFCBF',
-    $yellow300: '#FAF089',
-    $yellow400: '#F6E05E',
-    $yellow500: '#ECC94B',
-    $yellow600: '#D69E2E',
-    $yellow700: '#B7791F',
-    $yellow800: '#975A16',
-    $yellow900: '#744210',
-
-    $red100: '#FFF5F5',
-    $red200: '#FED7D7',
-    $red300: '#FEB2B2',
-    $red400: '#FC8181',
-    $red500: '#F56565',
-    $red600: '#E53E3E',
-    $red700: '#C53030',
-    $red800: '#9B2C2C',
-    $red900: '#742A2A',
-
-    $indigo100: '#EBF4FF',
-    $indigo200: '#C3DAFE',
-    $indigo300: '#A3BFFA',
-    $indigo400: '#7F9CF5',
-    $indigo500: '#667EEA',
-    $indigo600: '#5A67D8',
-    $indigo700: '#4C51BF',
-    $indigo800: '#434190',
-    $indigo900: '#3C366B',
-
-    $blue100: '#EBF8FF',
-    $blue200: '#BEE3F8',
-    $blue300: '#90CDF4',
-    $blue400: '#63B3ED',
-    $blue500: '#4299E1',
-    $blue600: '#3182CE',
-    $blue700: '#2B6CB0',
-    $blue800: '#2C5282',
-    $blue900: '#2A4365',
-  },
+  colors,
   fonts: {
     $default: 'Source Sans Pro, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, sans-serif',
     $mono: 'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
