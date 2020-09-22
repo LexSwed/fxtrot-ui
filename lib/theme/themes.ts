b370e0908b4040fba47ed59e5d30aacef862706c
Alexander Swed <lexswed@gmail.com>
2020-09-22 20:57:10 +0200

    fix themes

--- lib/theme/themes.ts
+++ lib/theme/themes.ts
@@ -1,17 +1,34 @@
 import * as palette from './palette';
 import { css } from '../stitches.config';
+import colors from './colors';
+
+const filtered = ['grey'];
 
 export const themes = Object.fromEntries(
-  Object.entries(palette).map(([name, colors]) => [
-    name,
-    css.theme({
-      colors: {
-        $primary: colors[`$${name}500` as keyof typeof colors],
-        $hover: colors[`$${name}600` as keyof typeof colors],
-        $active: colors[`$${name}700` as keyof typeof colors],
-      } as any,
-    }),
-  ])
+  Object.entries(palette)
+    .filter(([name]) => !filtered.includes(name))
+    .map(([name, colors]) => [
+      name,
+      css.theme({
+        colors: {
+          $primary: colors[`$${name}500` as keyof typeof colors],
+          $hover: colors[`$${name}600` as keyof typeof colors],
+          $active: colors[`$${name}700` as keyof typeof colors],
+        },
+      }),
+    ])
+    .concat([
+      [
+        'black',
+        css.theme({
+          colors: {
+            $primary: colors['$gray800'],
+            $hover: colors['$gray900'],
+            $active: 'black',
+          },
+        }),
+      ],
+    ])
 ) as Themes;
 
 type Themes = { [Shade in keyof typeof palette]: string };
