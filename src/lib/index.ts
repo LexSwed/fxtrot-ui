ab827141627339a4e04b8d4594a59f9fe99b8a1b
Alexander Swed <lexswed@gmail.com>
2020-10-12 12:21:07 +0200

    fix: expose StyleRecord type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-import { StylesWithVariants } from './types/helpers';
+import type { StylesWithVariants } from './types/helpers';
 
 export { default as Box } from './Box';
 export { default as Button } from './Button';
--- src/lib/types/helpers.d.ts
+++ src/lib/types/helpers.d.ts
@@ -1,10 +1,10 @@
 import type { TCssWithBreakpoints } from '@stitches/react';
 import type { TCssProperties, TCss } from '@stitches/core';
-import { css, styled } from '../stitches.config';
+import { css } from '../stitches.config';
 
 type Config = GetConfig<typeof css>;
 export type CssWithBreakpoints = TCssWithBreakpoints<Config>;
 export type StylesObject = TCssProperties<Config>;
-export type StylesWithVariants = Parameters<typeof styled>[1]['variants'];
+export type StylesWithVariants = TCssProperties<Config>;
 
 type GetConfig<S> = S extends TCss<infer T> ? T : never;
