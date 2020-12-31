57b7efd3b0e024f19261fdcae46200eb998039af
Alexander Swed <lexswed@gmail.com>
2020-12-31 16:32:16 +0100

    chore: update dokz 🎉 much faster TS 👏

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,31 @@
-import { DokzProvider, GithubLink } from 'dokz';
-import type { AppProps } from 'next/dist/next-server/lib/router/router';
+import React from 'react';
 import Head from 'next/head';
-import React, { Fragment } from 'react';
+import { DokzProvider, GithubLink } from 'dokz';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/dist/next-server/lib/router/router';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
 
   return (
-    <Fragment>
+    <>
       <Head>
         <link href="https://fonts.googleapis.com/css?family=Fira+Code" rel="stylesheet" key="google-font-Fira" />
       </Head>
-      <DokzProvider
-        headerItems={[<GithubLink key="0" url="https://github.com/LexSwed/fxtrot-ui" />]}
-        sidebarOrdering={{
-          'index.mdx': true,
-        }}
-        docsRootPath="pages"
-        githubUrl="LexSwed/fxtrot-ui"
-        branch="master"
-        headerLogo="Fxtrot UI"
-      >
-        <Component {...pageProps} />
-      </DokzProvider>
-    </Fragment>
+      <ChakraProvider resetCSS>
+        <DokzProvider
+          headerItems={[<GithubLink key="0" url="https://github.com/LexSwed/fxtrot-ui" />]}
+          sidebarOrdering={{
+            'index.mdx': true,
+          }}
+          docsRootPath="pages"
+          githubUrl="LexSwed/fxtrot-ui"
+          branch="master"
+          headerLogo="Fxtrot UI"
+        >
+          <Component {...pageProps} />
+        </DokzProvider>
+      </ChakraProvider>
+    </>
   );
 }
