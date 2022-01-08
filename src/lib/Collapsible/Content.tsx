e44a55312e576c40344fd715ff22877babdc9f2f
Oleksandr Shvechykov <oleksandrshvechykov@skyscanner.net>
2022-01-08 21:28:08 +0100

    fix(Collapsible): new collapsible component

---
+++ src/lib/Collapsible/Content.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import * as Rdx from '@radix-ui/react-collapsible';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
+
+import { styled } from '../stitches.config';
+import { useOpenState } from '../utils/OpenStateProvider';
+import type { CssStyles } from '../stitches.config';
+
+interface ContentProps extends React.ComponentProps<'div'> {
+  css?: CssStyles;
+}
+export const Content = React.forwardRef<HTMLDivElement, ContentProps>((props, ref) => {
+  return (
+    <Rdx.Content asChild forceMount>
+      <ContentInner {...props} ref={ref} />
+    </Rdx.Content>
+  );
+});
+
+const StyledContent = styled(motion.div, {});
+const ContentInner = React.forwardRef<HTMLDivElement, ContentProps>((props, ref) => {
+  const open = useOpenState();
+  return (
+    <AnimatePresence>
+      {open ? (
+        <StyledContent
+          {...(props as any)}
+          variants={contentVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          ref={ref}
+        />
+      ) : null}
+    </AnimatePresence>
+  );
+});
+const contentVariants: Variants = {
+  initial: { opacity: 0, y: -10, height: 0 },
+  animate: { opacity: 1, y: 0, height: 'auto', transition: { duration: 0.3 } },
+  exit: {
+    opacity: 0,
+    height: 0,
+    y: -10,
+    transition: {
+      duration: 0.2,
+    },
+  },
+};
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ export * from './Box';
 export * from './Button';
 export * from './Checkbox';
 export * from './CheckboxGroup';
+export * from './Collapsible';
 export * from './ComboBox';
 export * from './Dialog';
 export * from './Flex';
