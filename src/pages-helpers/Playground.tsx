6021558b208b627ab87bff7deb14e6bef933eb9e
Lex Swed <lexswed@gmail.com>
2022-03-21 21:31:31 +0100

    chore(docs): update playground

--- src/pages-helpers/Playground.tsx
+++ src/pages-helpers/Playground.tsx
@@ -2,7 +2,7 @@ import { Box, Icon, ToggleButton } from '@fxtrot/ui';
 import { SelectorIcon } from '@heroicons/react/outline';
 import { Root, Content, Trigger } from '@radix-ui/react-collapsible';
 import type { Language } from 'prism-react-renderer';
-import nightOwl from 'prism-react-renderer/themes/nightOwl';
+import vsLight from 'prism-react-renderer/themes/vsLight';
 
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import { components } from './MdxProvider';
@@ -14,15 +14,30 @@ type Props = {
 
 export const Playground = ({ code, language }: Props) => {
   return (
-    <LiveProvider theme={nightOwl} scope={components} code={`${code}`} language={language as Language}>
-      <Box boxShadow="$base" br="$base">
+    <LiveProvider theme={vsLight} scope={components} code={`${code}`} language={language as Language}>
+      <Box boxShadow="$popper" br="$base">
         <Root>
           <LivePreview
             // @ts-expect-error
             Component={Preview}
           />
           <Content asChild>
-            <Box css={{ borderBottomLeftRadius: '$base', borderBottomRightRadius: '$base', overflow: 'hidden' }}>
+            <Box
+              css={{
+                'borderTop': '1px solid $border--light',
+                '& *': {
+                  borderBottomLeftRadius: '$base',
+                  borderBottomRightRadius: '$base',
+                },
+                '& textarea': {
+                  'outline': 'none',
+                  'transition': '0.24s ease-in-out',
+                  '&:focus': {
+                    boxShadow: `0 0 0 2px inset $colors$border-accent`,
+                  },
+                },
+              }}
+            >
               <LiveEditor />
             </Box>
           </Content>
@@ -35,7 +50,7 @@ export const Playground = ({ code, language }: Props) => {
 
 const Preview: React.FC = ({ children }) => {
   return (
-    <Box position="relative" p="$4" pb="$12" bc="$surface">
+    <Box position="relative" p="$4" pb="$12" bc="$surface" br="$base">
       {children}
       <Box position="absolute" bottom="$2" right="$2">
         <Trigger asChild>
