342f472fed303741c80aa6d6190f89d05ad85117
Lex Swed <lexswed@gmail.com>
2022-04-02 23:54:05 +0200

    chore(docs): add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import type { AppProps } from 'next/dist/shared/lib/router/router';
 import { ThemeProvider } from '@fxtrot/ui';
+import { NextSeo } from 'next-seo';
 
 import { MdxProvider } from 'src/pages-helpers/MdxProvider';
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   return (
-    <ThemeProvider>
-      <MdxProvider>
-        <Component {...pageProps} />
-      </MdxProvider>
-    </ThemeProvider>
+    <>
+      <NextSeo
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: '/icons/favicon.ico',
+          },
+          {
+            rel: 'icon',
+            href: '/icons/favicon-32x32.png',
+            type: 'image/png',
+            sizes: '32x32',
+          },
+          {
+            rel: 'icon',
+            href: '/icons/favicon-16x16.png',
+            type: 'image/png',
+            sizes: '16x16',
+          },
+          {
+            rel: 'apple-touch-icon',
+            href: '/icons/apple-touch-icon.png',
+            sizes: '180x180',
+          },
+          {
+            rel: 'manifest',
+            href: '/icons/site.webmanifest',
+          },
+        ]}
+      />
+      <ThemeProvider>
+        <MdxProvider>
+          <Component {...pageProps} />
+        </MdxProvider>
+      </ThemeProvider>
+    </>
   );
 }
