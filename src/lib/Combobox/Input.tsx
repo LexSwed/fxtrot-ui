301471b90aa971bd81471a86ac729f7b72e87005
Lex Swed <lexswed@gmail.com>
2022-05-07 21:18:52 +0200

    fix: rename Combobox

---
+++ src/lib/Combobox/Input.tsx
@@ -0,0 +1,182 @@
+import React, { useRef } from 'react';
+import { SelectorIcon } from '@heroicons/react/solid';
+
+import { IconButton } from '../IconButton';
+import { Flex, FlexVariants } from '../Flex/Flex';
+import { FormField, Hint, HintBox, useFormField } from '../FormField/FormField';
+import { Icon } from '../Icon';
+import { Label } from '../Label';
+import { styled } from '../stitches.config';
+import { Tag } from '../Tag';
+import { InputField, InputProps } from '../TextField/shared';
+import { useAllHandlers, useForkRef, useKeyboardHandles } from '../utils/hooks';
+import { useOpenState, useOpenStateControls } from '../utils/OpenStateProvider';
+
+export interface Props
+  extends Omit<InputProps, 'onChange' | 'type' | 'value' | 'defaultValue' | 'children' | 'text' | 'as'>,
+    FlexVariants {
+  'label'?: string;
+  'secondaryLabel'?: string;
+  'hint'?: string;
+  'validity'?: 'valid' | 'invalid';
+  'inputRef'?: React.Ref<HTMLInputElement>;
+  'aria-controls'?: string;
+  /** Internal prop */
+  'hasNewBadge': boolean;
+  /** Internal prop */
+  'onSelect': () => void;
+  /** Internal prop */
+  'onFocusNext': () => void;
+  /** Internal prop */
+  'onFocusPrev': () => void;
+  /** Internal prop */
+  'value': string;
+  /** Internal prop */
+  'onChange': (newValue: string) => void;
+}
+
+export const ComboboxInput: React.FC<Props> = ({
+  label,
+  secondaryLabel,
+  hint,
+  main,
+  cross,
+  flow,
+  display,
+  gap,
+  css,
+  style,
+  className,
+  validity,
+  disabled,
+  variant = 'boxed',
+  id,
+  inputRef,
+  /** custom props, should be filtered from Combobox parent */
+  hasNewBadge,
+  value,
+  onChange,
+  onSelect,
+  onFocusNext,
+  onFocusPrev,
+  ...props
+}) => {
+  const ariaProps = useFormField({ id, hint });
+  const isOpen = useOpenState();
+  const { open, close } = useOpenStateControls();
+  const innerRef = useRef<HTMLInputElement>(null);
+  const refs = useForkRef(inputRef, innerRef);
+
+  const handleChange = useAllHandlers((e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value), open);
+  const keydownControls = useKeyboardHandles({
+    'ArrowDown': () => {
+      open();
+      onFocusNext();
+    },
+    'ArrowUp': () => {
+      open();
+      onFocusPrev();
+    },
+    'Escape': close,
+    'Tab.propagate': close,
+    'Enter': isOpen ? onSelect : undefined,
+    'Space': isOpen ? onSelect : undefined,
+  });
+
+  const handleKeyDown = useAllHandlers(keydownControls, props.onKeyDown);
+
+  return (
+    <FormField
+      main={main}
+      cross={cross}
+      flow={flow}
+      display={display}
+      gap={gap}
+      css={css}
+      style={style}
+      className={className}
+      hasHint={!!hint}
+    >
+      {label !== undefined && (
+        <Label label={label} secondary={secondaryLabel} htmlFor={ariaProps.id} disabled={disabled} />
+      )}
+      <HintBox>
+        <InputFieldWrapper
+          {...props}
+          {...ariaProps}
+          aria-expanded={isOpen}
+          aria-autocomplete="list"
+          role="combobox"
+          autoComplete="off"
+          spellCheck="false"
+          disabled={disabled}
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          type="text"
+          variant={variant}
+          hasNewBadge={hasNewBadge}
+          validity={validity}
+          inputRef={refs}
+        >
+          <ComboboxButton hasNewBadge={hasNewBadge} inputRef={innerRef} />
+        </InputFieldWrapper>
+        {hint && (
+          <Hint id={ariaProps['aria-describedby']} validity={validity}>
+            {hint}
+          </Hint>
+        )}
+      </HintBox>
+    </FormField>
+  );
+};
+
+const InputFieldWrapper = styled(InputField, {
+  variants: {
+    hasNewBadge: {
+      true: {
+        pr: '$16',
+      },
+    },
+  },
+});
+
+const ButtonContainer = styled(Flex, {
+  position: 'absolute',
+  right: 0,
+  top: 0,
+  height: '100%',
+  px: '$1',
+});
+
+const ComboboxButton = React.memo(
+  ({ hasNewBadge, inputRef }: { hasNewBadge: boolean; inputRef: React.RefObject<HTMLInputElement> }) => {
+    const isOpen = useOpenState();
+    const { toggle } = useOpenStateControls();
+
+    return (
+      <ButtonContainer cross="center">
+        {hasNewBadge && <Tag label="NEW" size="sm" />}
+        <IconButton
+          variant="transparent"
+          tabIndex={-1}
+          aria-hidden={isOpen}
+          aria-expanded={isOpen}
+          label="Open the list"
+          onPointerDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            toggle();
+            inputRef.current?.focus();
+            inputRef.current?.select();
+          }}
+          size="xs"
+        >
+          <Icon as={SelectorIcon} />
+        </IconButton>
+      </ButtonContainer>
+    );
+  }
+);
+
+ComboboxButton.displayName = 'ComboboxButton';
---
+++ src/lib/Combobox/atoms.tsx
@@ -0,0 +1,40 @@
+import { Provider, atom, useAtom } from 'jotai';
+import React from 'react';
+
+import { useAtomValue, useUpdateAtom } from 'jotai/utils';
+import { useEffect } from 'react';
+
+const innerValue = atom<string | null>(null);
+const fxtrotScope = Symbol('Combobox scope');
+
+interface SyncedValue {
+  (propValue?: string | null, propOnChange?: (newValue: string | null) => void): readonly [
+    value: string | null,
+    setValue: (newValue: string | null) => void
+  ];
+}
+export const useSyncValue: SyncedValue = (propValue, onChange = () => {}) => {
+  const [value, setValue] = useAtom(innerValue, fxtrotScope);
+
+  useEffect(() => {
+    if (typeof propValue !== 'undefined') {
+      setValue(propValue);
+    }
+  }, [propValue, setValue]);
+
+  if (typeof propValue !== 'undefined') {
+    return [propValue, onChange] as const;
+  }
+
+  return [value, setValue] as const;
+};
+
+const focusedItemId = atom<string | null>(null);
+export const useFocusedItemId = () => useAtom(focusedItemId, fxtrotScope);
+export const useItemSelected = (value: string) => value === useAtomValue(innerValue, fxtrotScope);
+export const useItemFocused = (id: string) => id === useAtomValue(focusedItemId, fxtrotScope);
+export const useFocusItem = () => useUpdateAtom(focusedItemId, fxtrotScope);
+
+export const StateProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+  return <Provider scope={fxtrotScope}>{children}</Provider>;
+};
