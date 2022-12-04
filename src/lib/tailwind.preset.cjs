1ae231db8ec6324d02f9da096a71dfcea11daf2e
LexSwed <lexswed@gmail.com>
2022-12-04 17:07:12 +0100

    add setup

---
+++ src/lib/tailwind.preset.cjs
@@ -0,0 +1,100 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin')
+const colors = require('./colors.cjs')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      opacity: {
+        15: '0.15',
+      },
+      boxShadow: {
+        popper: `0 0 2px rgb(0 0 0 / 0.3), ${defaultTheme.boxShadow.xl}`,
+      },
+      transitionDuration: {
+        240: '240ms'
+      }
+    },
+    spacing: {
+      ...defaultTheme.spacing,
+      none: defaultTheme.spacing['0'],
+      xs: defaultTheme.spacing['1'],
+      sm: defaultTheme.spacing['2'],
+      md: defaultTheme.spacing['6'],
+      lg: defaultTheme.spacing['8'],
+      xl: defaultTheme.spacing['10'],
+      base: defaultTheme.spacing['10'],
+    },
+    fontFamily: {
+      sans: ['Inter', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'sans-serif'],
+      mono: ['Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace'],
+    },
+    colors,
+  },
+  plugins: [plugin(logicalPropertiesPlugin)],
+};
+
+
+/** 
+ * Overrides default -top, -bottom, etc with CSS Logical Properties
+ * @type {import('tailwindcss/plugin')} */
+function logicalPropertiesPlugin({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      'py': (value) => ({
+        paddingBlock: value
+      }),
+      'px': (value) => ({
+        paddingInline: value
+      })
+    },
+    { values: theme('padding') }
+  );
+  matchUtilities(
+    {
+      'my': (value) => ({
+        marginBlock: value
+      }),
+      'mx': (value) => ({
+        marginInline: value
+      })
+    },
+    { values: theme('margin') }
+  );
+  matchUtilities(
+    {
+      'pt': (value) => ({
+        paddingBlockStart: value
+      }),
+      'pb': (value) => ({
+        paddingBlockEnd: value
+      }),
+      'pl': (value) => ({
+        paddingInlineStart: value
+      }),
+      'pr': (value) => ({
+        paddingInlineEnd: value
+      })
+    },
+    { values: theme('padding') }
+  );
+  matchUtilities(
+    {
+      'mt': (value) => ({
+        marginBlockStart: value
+      }),
+      'mb': (value) => ({
+        marginBlockEnd: value
+      }),
+      'ml': (value) => ({
+        marginInlineStart: value
+      }),
+      'mr': (value) => ({
+        marginInlineEnd: value
+      })
+    },
+    { values: theme('margin') }
+  );
+} 
