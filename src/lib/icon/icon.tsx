cafa22b55aef57b1f3f6a893a1123883ec8dcc69
LexSwed <lexswed@gmail.com>
2022-12-09 10:25:09 +0100

    button updates

--- src/lib/icon/icon.tsx
+++ src/lib/icon/icon.tsx
@@ -2,6 +2,8 @@ import { classed as css, VariantProps } from '@tw-classed/core';
 import { clsx } from 'clsx';
 import { type ComponentProps, type ElementType, forwardRef } from 'react';
 
+import styles from './icon.module.css';
+
 interface Props extends ComponentProps<'svg'>, VariantProps<typeof iconCss> {
   as: ElementType;
 }
@@ -18,20 +20,20 @@ const Icon = forwardRef<SVGSVGElement, Props>(({ color, as: Svg, className, size
   );
 });
 
-const iconCss = css('inline-block flex-shrink-0 flex-grow-0', {
+const iconCss = css(styles.icon, {
   variants: {
     size: {
-      'xs': 'size-3.5 stroke-[3px]',
-      'sm': 'size-4',
-      'md': 'size-5',
-      'lg': 'size-6',
-      'xl': 'size-7',
-      '2xl': 'size-8',
-      '3xl': 'size-12',
-      '4xl': 'size-16',
-      '5xl': 'size-20',
-      '6xl': 'size-24',
-      'inherit': 'size-[1em]',
+      'xs': styles['icon--xs'],
+      'sm': styles['icon--sm'],
+      'md': styles['icon--md'],
+      'lg': styles['icon--lg'],
+      'xl': styles['icon--xl'],
+      '2xl': styles['icon--2xl'],
+      '3xl': styles['icon--3xl'],
+      '4xl': styles['icon--4xl'],
+      '5xl': styles['icon--5xl'],
+      '6xl': styles['icon--6xl'],
+      'inherit': styles['icon--inherit'],
     },
   },
 });
