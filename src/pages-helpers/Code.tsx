d94f6a0d12acc5a47a035d5919c7aef27e4d3634
LexSwed <lexswed@gmail.com>
2022-12-26 12:53:05 +0100

    chore(docs): inline code visual update

--- src/pages-helpers/Code.tsx
+++ src/pages-helpers/Code.tsx
@@ -1,6 +1,6 @@
 export const Code = ({ children }: { children: string }) => {
   return (
-    <code className="rounded-xs -m-[2px] inline-block whitespace-pre-wrap bg-primary/10 p-[2px] align-middle text-[0.8em] leading-[1.1em] text-primary">
+    <code className="rounded-xs -m-[2px] inline-block whitespace-pre-wrap bg-primary/10 align-middle text-[0.8em] leading-[1.5em] text-primary px-0.5">
       {children}
     </code>
   );
