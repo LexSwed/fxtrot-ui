c09570b01b4fb07eb5af64f66f39a2f5024bcbd1
LexSwed <lexswed@gmail.com>
2022-12-31 16:55:46 +0100

    fix: borderRadius type

--- src/lib/theme-provider/types.ts
+++ src/lib/theme-provider/types.ts
@@ -86,41 +86,6 @@ export type Theme = {
   };
   // Tailwind default + xs, sm, md, etc
   spacing?: {
-    px?: string;
-    0?: string;
-    0.5?: string;
-    1?: string;
-    1.5?: string;
-    2?: string;
-    2.5?: string;
-    3?: string;
-    3.5?: string;
-    4?: string;
-    5?: string;
-    6?: string;
-    7?: string;
-    8?: string;
-    9?: string;
-    10?: string;
-    11?: string;
-    12?: string;
-    14?: string;
-    16?: string;
-    20?: string;
-    24?: string;
-    28?: string;
-    32?: string;
-    36?: string;
-    40?: string;
-    44?: string;
-    48?: string;
-    52?: string;
-    56?: string;
-    60?: string;
-    64?: string;
-    72?: string;
-    80?: string;
-    96?: string;
     xs?: string;
     sm?: string;
     md?: string;
@@ -147,5 +112,16 @@ export type Theme = {
     '7xl'?: [fontSize: string, lineHeight: string];
     '8xl'?: [fontSize: string, lineHeight: string];
     '9xl'?: [fontSize: string, lineHeight: string];
+  };
+  borderRadius?: {
+    'none'?: string;
+    'sm'?: string;
+    'DEFAULT'?: string;
+    'md'?: string;
+    'lg'?: string;
+    'xl'?: string;
+    '2xl'?: string;
+    '3xl'?: string;
+    'full'?: string;
   };
 };
