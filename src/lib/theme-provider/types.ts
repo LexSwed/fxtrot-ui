cd2ef40d30dca6a51f782e3e64f2118f04652eab
LexSwed <lexswed@gmail.com>
2022-12-31 16:48:32 +0100

    fix: fontSize Theme types

--- src/lib/theme-provider/types.ts
+++ src/lib/theme-provider/types.ts
@@ -134,18 +134,18 @@ export type Theme = {
   };
   /** fontSize + lineHeight */
   fontSize?: {
-    'xs'?: ['0.75rem', '1rem'];
-    'sm'?: ['0.875rem', '1.25rem'];
-    'md'?: ['1rem', '1.5rem'];
-    'lg'?: ['1.125rem', '1.75rem'];
-    'xl'?: ['1.25rem', '1.75rem'];
-    '2xl'?: ['1.5rem', '2rem'];
-    '3xl'?: ['1.875rem', '2.25rem'];
-    '4xl'?: ['2.25rem', '2.5rem'];
-    '5xl'?: ['3rem', '1'];
-    '6xl'?: ['3.75rem', '1'];
-    '7xl'?: ['4.5rem', '1'];
-    '8xl'?: ['6rem', '1'];
-    '9xl'?: ['8rem', '1'];
+    'xs'?: [fontSize: string, lineHeight: string];
+    'sm'?: [fontSize: string, lineHeight: string];
+    'md'?: [fontSize: string, lineHeight: string];
+    'lg'?: [fontSize: string, lineHeight: string];
+    'xl'?: [fontSize: string, lineHeight: string];
+    '2xl'?: [fontSize: string, lineHeight: string];
+    '3xl'?: [fontSize: string, lineHeight: string];
+    '4xl'?: [fontSize: string, lineHeight: string];
+    '5xl'?: [fontSize: string, lineHeight: string];
+    '6xl'?: [fontSize: string, lineHeight: string];
+    '7xl'?: [fontSize: string, lineHeight: string];
+    '8xl'?: [fontSize: string, lineHeight: string];
+    '9xl'?: [fontSize: string, lineHeight: string];
   };
 };
