9627fe61fb1b0ac71ca81abae744b4bbc87f6d08
LexSwed <lexswed@gmail.com>
2023-01-08 13:00:40 +0100

    fix: tabs overflow

--- src/pages-helpers/Code.tsx
+++ src/pages-helpers/Code.tsx
@@ -1,6 +1,6 @@
 export const Code = ({ children }: { children: string }) => {
   return (
-    <code className="rounded-xs -m-[2px] inline-block whitespace-pre-wrap bg-primary/10 align-middle text-[0.8em] leading-[1.5em] text-primary px-0.5">
+    <code className="rounded-xs -m-[2px] inline-block whitespace-pre-wrap bg-primary/10 align-middle font-mono text-[0.8em] leading-[1.5em] text-primary px-0.5">
       {children}
     </code>
   );
