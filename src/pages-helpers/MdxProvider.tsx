ac10d414cedb47a310caccbd59e0d2bafd7005f7
LexSwed <lexswed@gmail.com>
2023-04-15 10:59:04 +0200

    chore: fix heroicons and react-live issue

--- src/pages-helpers/MdxProvider.tsx
+++ src/pages-helpers/MdxProvider.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import Image from 'next/image';
 import { BsTypeBold, BsTypeItalic, BsTypeUnderline } from 'react-icons/bs';
-import * as HeroIcons from '@heroicons/react/24/outline';
+// eslint-disable-next-line import/default
+import HeroIcons from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { clsx } from 'clsx';
 import * as FxtrotUI from '@fxtrot/ui';
