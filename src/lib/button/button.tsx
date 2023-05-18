932303a8c8225b9587dd222c3d0f50f8a72d1529
LexSwed <lexswed@gmail.com>
2023-05-18 20:11:19 +0200

    fix(Button): revert icon button changes

--- src/lib/button/button.tsx
+++ src/lib/button/button.tsx
@@ -1,6 +1,8 @@
 import { classed as css, type VariantProps } from '@tw-classed/core';
-import { type ComponentProps, type ElementType, forwardRef } from 'react';
+import { type ComponentProps, type ElementType, forwardRef, Children } from 'react';
+import cx from 'clsx';
 
+import { isValidElement } from 'react';
 import { flexCss, type FlexVariants } from '../flex/flex';
 import { Icon } from '../icon';
 
@@ -18,7 +20,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   return (
     <button
       {...rest}
-      className={buttonCss(props)}
+      className={cx(buttonCss(props), isIconButton(props) && styles['button--icon'])}
       aria-label={label}
       title={label}
       disabled={disabled}
@@ -27,7 +29,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
       ref={ref}
     >
       {children}
-      {icon ? <Icon as={icon} size={size} className={styles.icon} /> : null}
+      {icon ? <Icon as={icon} size={size} /> : null}
     </button>
   );
 });
@@ -39,12 +41,30 @@ interface LinkButtonProps extends ButtonOwnProps, ComponentProps<'a'> {}
 const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>((props, ref) => {
   const { icon, label, size = 'md', children, ...rest } = props;
   return (
-    <a {...rest} className={buttonCss(props)} aria-label={label} title={label} ref={ref}>
+    <a
+      {...rest}
+      className={cx(buttonCss(props), isIconButton(props) && styles['button--icon'])}
+      aria-label={label}
+      title={label}
+      ref={ref}
+    >
       {icon ? <Icon as={icon} size={size} /> : null}
       {children}
     </a>
   );
 });
+
+/**
+ * Checks if the Button has only icon inside.
+ * CSS-only :has(>svg:only-child) would not count text nodes.
+ */
+function isIconButton(props: ButtonProps | LinkButtonProps) {
+  // icon passed as props
+  if (props.icon) return Children.count(props.children) === 0;
+
+  // icon used as children
+  return Children.count(props.children) === 1 && isValidElement(props.children) && props.children.type === Icon;
+}
 
 const buttonCss = css(styles.button, flexCss, {
   variants: {
